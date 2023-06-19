5eb77d625abab1221a957ec110207a8d39595c4a
Pierre TOP (TOPI) <ptop@octo.com>
2023-06-19 12:07:48 +0200

    Configure lint

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
-import * as chai from 'chai';
-import { expect } from 'chai';
+import chai from 'chai';
+const expect = chai.expect;
 import sinonChai from 'sinon-chai';
 chai.use(sinonChai);
+
 
 it('fails', function() {
   expect(1).to.equal(0);
