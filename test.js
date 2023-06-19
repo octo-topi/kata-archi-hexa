4db65e3e6a914461b80f7f74d4b391d6f307e100
Pierre TOP (TOPI) <ptop@octo.com>
2023-06-19 11:56:50 +0200

    Add chai plugin for eslint

--- test.js
+++ test.js
@@ -1,4 +1,7 @@
-const { expect } = require('chai');
+const  chai = require('chai');
+const { expect }= require('chai');
+const sinonChai = require('sinon-chai');
+chai.use(sinonChai);
 
 it('fails', function() {
   expect(1).to.equal(0);
