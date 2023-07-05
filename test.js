15a782ba954c4c90585c4d0ee144cd3869bdcd3c
Pierre TOP (TOPI) <ptop@octo.com>
2023-07-05 09:08:48 +0200

    add sinon assertions

--- test.js
+++ test.js
@@ -1,16 +1,31 @@
 import chai from 'chai';
 const expect = chai.expect;
+import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 chai.use(sinonChai);
 
-describe('When a test is written',function() {
-  it('should fail', function() {
+describe('#stub',function() {
+  it("should return {people: 'jane'}", function() {
     // given
+    const stub = sinon.stub();
+    stub.returns({people: 'jane'})
 
     // when
+    const data = stub()
 
     // then
-    expect(1).to.equal(0);
+    expect(data).to.deep.equal({people: 'jane'});
+  });
+  it("should be called once with {parameter: 'foo'}", function() {
+    // given
+    const stub = sinon.stub();
+    stub.returns({people: 'jane'})
+
+    // when
+    stub({parameter: 'foo'})
+
+    // then
+    expect(stub).to.have.been.calledWith({parameter: 'foo'});
   });
 })
 
